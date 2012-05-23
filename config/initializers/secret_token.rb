dea4b83a3446a379d3e941fa351467da9088e684
art devil <artdvl91@gmail.com>
2012-05-23 18:21:52 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wrdelivery::Application.config.secret_token = '87ba79d2c9f5a2a76ba895059eaa4992c2d8bf7b5bca1a30a6065ce351354fcc290f87cbe46310c9968ab3b365d483051d25a7d89471bcff6154b33b2536fd2d'
